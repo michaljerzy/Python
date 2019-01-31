132151af14492b5b4f6d11184d917ce5eb5a51c6
Michał Andrzejewski <michalandrzejewski@localhost.localdomain>
2019-01-31 12:46:03 +0100

    first commit

---
+++ petlafor.py
@@ -0,0 +1,15 @@
+for x in range(1,20):
+	print (x)
+	if(x==10):
+		break
+
+numbers = [1,2,3,4,5,6]
+
+for number in numbers:
+	print(number)
+
+slowa = ['chuj','chuj','chuj','chuj']
+
+for litera in slowa:
+	for i in litera:
+		print(i)
