4fc5bacec4de168d7963e3797c8e04dfae526524
Michał <michal.jerzy99@gmail.com>
2020-03-27 22:13:49 +0100

    deleted

--- Jupiter/sprawdzanie.py
+++
@@ -1,9 +0,0 @@
-from math import sqrt
-
-def isPrime(n):
-    return n > 1 and all(n % d != 0 for d in range(2, int(sqrt(n))))
-
-def enumPrimes(start, end):
-    return [ n for n in range(start, end) if isPrime(n) ]
-
-print ('Liczby pierwsze: ', enumPrimes(10, 100))
